bd8a50461bf87c4fa1a9a62e78a626a44f477b03
kelechiokpani <mremmatex@gmail.com>
2026-06-27 13:44:14 +0100

    fix(events): use Node.js built-in EventEmitter to resolve module resolution issue

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,42 +1,78 @@
-import Redis from 'ioredis';
-import { env } from './env';
-import { logger } from '../shared/utils/logger';
+import Redis from "ioredis";
+import { env } from "./env";
+import { logger } from "@shared/utils/logger";
+
+
+export const redis = env.REDIS_URL
+  ? new Redis(env.REDIS_URL, {
+      connectTimeout: 10000,
+      maxRetriesPerRequest: null,
+
+      retryStrategy(times) {
+        return Math.min(times * 100, 3000);
+      },
+    })
+  : new Redis({
+      host: env.REDIS_HOST,
+      port: Number(env.REDIS_PORT),
+      password: env.REDIS_PASSWORD || undefined,
+
+      connectTimeout: 10000,
+      maxRetriesPerRequest: null,
+
+      retryStrategy(times) {
+        return Math.min(times * 100, 3000);
+      },
+    });
+
+redis.on("connect", () => {
+  console.log("✅ Redis connected");
+  logger.info("✅ Redis connected");
+});
+
+redis.on("ready", () => {
+  console.log("✅ Redis ready");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis error", err);
+  logger.error("❌ Redis error:", err);
+});
+
+export default redis;
+
+// import Redis from 'ioredis';
+// import { env } from './env';
+// import { logger } from '../shared/utils/logger';
+
+
 
 // export const redis = new Redis({
 //   host: env.REDIS_HOST,
-//   port: env.REDIS_PORT,
+//   port: Number(env.REDIS_PORT || env.REDIS_URL),
 //   password: env.REDIS_PASSWORD || undefined,
-//   retryStrategy: (times) => Math.min(times * 50, 2000),
+
+//   connectTimeout: 10000,
 //   maxRetriesPerRequest: null,
+
+//   retryStrategy(times) {
+//     return Math.min(times * 100, 3000);
+//   },
 // });
 
+// redis.on('connect', () => {
+//   console.log('✅ Redis connected');
+// });
 
-export const redis = new Redis({
-  host: env.REDIS_HOST,
-  port: Number(env.REDIS_PORT),
-  password: env.REDIS_PASSWORD || undefined,
+// redis.on('ready', () => {
+//   console.log('✅ Redis ready');
+// });
 
-  connectTimeout: 10000,
-  maxRetriesPerRequest: null,
+// redis.on('error', (err) => {
+//   console.error('❌ Redis error', err);
+// });
 
-  retryStrategy(times) {
-    return Math.min(times * 100, 3000);
-  },
-});
+// redis.on('connect', () => logger.info('✅ Redis connected'));
+// redis.on('error', (err) => logger.error('❌ Redis error:', err));
 
-redis.on('connect', () => {
-  console.log('✅ Redis connected');
-});
-
-redis.on('ready', () => {
-  console.log('✅ Redis ready');
-});
-
-redis.on('error', (err) => {
-  console.error('❌ Redis error', err);
-});
-
-redis.on('connect', () => logger.info('✅ Redis connected'));
-redis.on('error', (err) => logger.error('❌ Redis error:', err));
-
-export default redis;
+// export default redis;
